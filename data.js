6f1f4f22adc3c685b548560575c2c3800d296cc3
nandodoria <nandodoria@hotmail.ca>
2019-03-13 13:49:38 -0400

    more stuff

---
+++ data.js
@@ -0,0 +1,21 @@
+const data = {
+    name: "Fernando",
+    role: "designer",
+
+
+    user1: {
+        name: "user",
+        role: "developer",
+        bio: "some bio info goes here",
+        bioPic: ""
+    },
+
+    user2: {
+        name: "user2",
+        role: "video",
+        bio: "some bio info goes here",
+        bioPic: ""
+    }
+};
+
+module.exports = data;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+const data = require(`../data`);
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express', message: "Welcome to your first express app" });
+});
+
+router.get('/:user', function(req, res, next) {
+  console.log('data:', data);
+  console.log('user:', req.params.user);
+  res.render('userpage', data[req.params.user]);
 });
 
 module.exports = router;
